79ce92bd4f6b12e2f1f00632cd954b1b6833741d
ISRAEL_OBK <87314858+israelobk@users.noreply.github.com>
2024-01-27 23:52:09 +0100

    Array operations

--- DataStructureAlgorithm/Program.cs
+++ DataStructureAlgorithm/Program.cs
@@ -16,7 +16,7 @@ length++;
 
 
 
-// inserting 8 at the length of every 2 index
+/*// inserting 8 at the length of every 2 index
 int[] ntArray1 = new int[6];
 
 int length1 = 2;
@@ -25,10 +25,10 @@ for (int i = 0; i < 5; i++)
 {
     ntArray1[length1] = 8;
     length1++;
-    /*    ntArray[length] = i + 1;
-        length++;*/
+    *//*    ntArray[length] = i + 1;
+        length++;*//*
 }
-
+*/
 
 
 
@@ -37,8 +37,21 @@ int[] ntArray2 = new int[6];
 
 int length2 = 0;
 
-for (int i = 0; i < 2; i++)
+for (int i = 0; i <= 2; i++)
 {
     ntArray2[length2] = 8;
     length2++;
 }
+
+// Inserting at the start of an array
+int[] ntArray3 = new int[6];
+
+for (int i = 3; i >= 0; i--)
+{
+    //this is moving over all the values
+    ntArray3[i] = ntArray3[i];
+
+}
+ntArray3[0] = 20;
+
+int value = 0;
