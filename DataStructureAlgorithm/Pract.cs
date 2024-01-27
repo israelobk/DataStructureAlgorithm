63363b340eefda4f7a2efd4892830583ecce0692
ISRAEL_OBK <87314858+israelobk@users.noreply.github.com>
2024-01-27 19:52:41 +0100

    Add project files.

---
+++ DataStructureAlgorithm/Pract.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgorithm
+{
+    internal class Pract
+    {
+        public int arr()
+        {
+            int[] ntArray = new int[6];
+
+            int length = 2;
+
+            for (int i = 0; i < 5; i++)
+            {
+                ntArray[length] = 8;
+                length++;
+                /*    ntArray[length] = i + 1;
+                    length++;*/
+            }
+
+
+            return 0;
+        }
+    }
+}
---
+++ DataStructureAlgorithm/Program.cs
@@ -0,0 +1,44 @@
+﻿
+//inserting at the end of an array
+int[] ntArray = new int[6];
+
+int length  = 0;
+
+for (int i = 0; i < 3; i++)
+{
+    ntArray[length] = i;
+    length++;
+}
+
+ntArray[length] = 8;
+length++;
+
+
+
+
+// inserting 8 at the length of every 2 index
+int[] ntArray1 = new int[6];
+
+int length1 = 2;
+
+for (int i = 0; i < 5; i++)
+{
+    ntArray1[length1] = 8;
+    length1++;
+    /*    ntArray[length] = i + 1;
+        length++;*/
+}
+
+
+
+
+// Add the the first three Index
+int[] ntArray2 = new int[6];
+
+int length2 = 0;
+
+for (int i = 0; i < 2; i++)
+{
+    ntArray2[length2] = 8;
+    length2++;
+}
